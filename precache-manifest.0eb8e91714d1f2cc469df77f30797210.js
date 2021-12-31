3e48d7a5a8c38539b4b13251abeeb66abbe8f237
Steven Boutcher <boutchersj@gmail.com>
2021-12-30 21:44:12 -0600

    [MAIN] Updates

---
+++ precache-manifest.0eb8e91714d1f2cc469df77f30797210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe57882972268368ac3d108e4888913",
+    "url": "/birthday_helper/index.html"
+  },
+  {
+    "revision": "bfb359aae52d6d6ceff3",
+    "url": "/birthday_helper/static/css/main.b381a68e.chunk.css"
+  },
+  {
+    "revision": "3c8b47c24cd3cdcdccb9",
+    "url": "/birthday_helper/static/js/2.97d315f0.chunk.js"
+  },
+  {
+    "revision": "3da27f1557990189b625b605ce8d60dc",
+    "url": "/birthday_helper/static/js/2.97d315f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb359aae52d6d6ceff3",
+    "url": "/birthday_helper/static/js/main.8809004c.chunk.js"
+  },
+  {
+    "revision": "d74d83be29fcba192baa",
+    "url": "/birthday_helper/static/js/runtime-main.792c9bb6.js"
+  }
+]);
